03be3c44834abdba01724e5d720d443914c5c45d
Rodney Hayes <Rodney.Hayes@allgress.com>
2021-12-09 14:20:55 -0800

    Unicafe step 3 complete

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,10 +1,6 @@
 import React, {useState} from 'react';
 
-const App = () => {
-	const [good, setGood] = useState(0);
-	const [neutral, setNeutral] = useState(0);
-	const [bad, setBad] = useState(0);
-
+const Statistics = ({good, neutral, bad}) => {
 	const getTotalFeedbackCount = () => {
 		return good + neutral + bad;
 	}
@@ -18,6 +14,26 @@ const App = () => {
 	}
 
 	return (
+		<div>
+			<h3>Statistics</h3>
+			Good: {good}<br/>
+			Neutral: {neutral}<br/>
+			Bad: {bad}<br/>
+			All: {getTotalFeedbackCount()}<br/>
+			Average: {getAverageScore()}<br/>
+			Positive: {getPositivePercentage()}<br/>
+		</div>
+	)
+}
+
+const App = () => {
+	const [good, setGood] = useState(0);
+	const [neutral, setNeutral] = useState(0);
+	const [bad, setBad] = useState(0);
+
+	
+
+	return (
 		<>
 			<div>
 				<h3>Give Feedback</h3>
@@ -25,15 +41,7 @@ const App = () => {
 				<button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
 				<button onClick={() => setBad(bad + 1)}>Bad</button>
 			</div>
-			<div>
-				<h3>Statistics</h3>
-				Good: {good}<br/>
-				Neutral: {neutral}<br/>
-				Bad: {bad}<br/>
-				All: {getTotalFeedbackCount()}<br/>
-				Average: {getAverageScore()}<br/>
-				Positive: {getPositivePercentage()}<br/>
-			</div>
+			<Statistics good={good} neutral={neutral} bad={bad}/>
 		</>
 	)
 }
