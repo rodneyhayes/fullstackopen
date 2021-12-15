0298a1e3e8edd156c4190c4c6b3d2e0f90a8f2c0
Rodney Hayes <Rodney.Hayes@allgress.com>
2021-12-14 20:08:54 -0800

    Part 2 course information step 9 complete

--- part2/course-information/src/App.js
+++ part2/course-information/src/App.js
@@ -40,31 +40,54 @@ const Content = ({ course }) => {
 }
 
 const App = () => {
-	const course = {
-		id: 1,
-		name: 'Half Stack Application Development',
-		parts: [
+	const courses = [
+		{
+		  name: 'Half Stack application development',
+		  id: 1,
+		  parts: [
 			{
-				id: 1,
-				name: 'Fundamentals of React',
-				exercises: 10
+			  name: 'Fundamentals of React',
+			  exercises: 10,
+			  id: 1
 			},
 			{
-				id: 2,
-				name: 'Using props to pass data',
-				exercises: 7
+			  name: 'Using props to pass data',
+			  exercises: 7,
+			  id: 2
 			},
 			{
-				id: 3,
-				name: 'State of a component',
-				exercises: 14
+			  name: 'State of a component',
+			  exercises: 14,
+			  id: 3
+			},
+			{
+			  name: 'Redux',
+			  exercises: 11,
+			  id: 4
 			}
-		]
-	}
+		  ]
+		}, 
+		{
+		  name: 'Node.js',
+		  id: 2,
+		  parts: [
+			{
+			  name: 'Routing',
+			  exercises: 3,
+			  id: 1
+			},
+			{
+			  name: 'Middlewares',
+			  exercises: 7,
+			  id: 2
+			}
+		  ]
+		}
+	  ]
 
 	return (
 		<>
-			<Course course={course}/>
+			{courses.map(course => <Course key={course.id} course={course}/>)}
 		</>
 	)
 }
