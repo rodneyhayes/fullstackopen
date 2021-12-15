adce36aa94a718596948ec169a8d0217d0e403f8
Rodney Hayes <Rodney.Hayes@allgress.com>
2021-12-14 20:11:43 -0800

    Part 2 2.5 complete

--- part2/course-information/src/App.js
+++ part2/course-information/src/App.js
@@ -1,43 +1,5 @@
 import React from 'react';
-
-const Course = ({course}) => {
-	return (
-		<div>
-			<Header course={course} />
-			<Content course={course} />
-			<Total parts={course.parts} />
-		</div>
-	)
-}
-
-const Header = ({ course }) => {
-	return (
-		<h1>{course.name}</h1>
-	)
-}
-
-const Total = ({ parts }) => {
-	const sum = parts.reduce((sum, part) => sum + part.exercises, 0);
-	return (
-		<p>Total number of exercises: {sum}</p>
-	)
-}
-
-const Part = (props) => {
-	return (
-		<p>
-			{props.part.name} {props.part.exercises}
-		</p>
-	)
-}
-
-const Content = ({ course }) => {
-	return (
-		<div>
-			{course.parts.map(part => <Part key={part.id} part={part}/>)}
-		</div>
-	)
-}
+import Course from './components/Course';
 
 const App = () => {
 	const courses = [
